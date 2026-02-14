089e9bf62cde7a04801b1946a52c68e9b1ea74b0
CliffKoes <cliffkoes@gmail.com>
2026-02-14 21:17:54 +0800

    Add Sword

--- Assets/Scripts/Weapons/SwordWeapon.cs
+++ Assets/Scripts/Weapons/SwordWeapon.cs
@@ -7,12 +7,11 @@ public class SwordWeapon : WeaponBase
 
     public float damage = 50f;
 
-    private Collider2D[] _results;
     public override void Attack(Player attacker)
     {
-        var size = Physics2D.OverlapCircleNonAlloc(attacker.shootPoint.position, 2f, _results, enemyMask);
+        var results = Physics2D.OverlapCircleAll(attacker.shootPoint.position, 2f, enemyMask);
 
-        foreach (var hit in _results)
+        foreach (var hit in results)
         {
             if (hit.CompareTag("Enemy"))
             {
